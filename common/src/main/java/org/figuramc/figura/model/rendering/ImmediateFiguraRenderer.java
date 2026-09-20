0d1223dc1fa75d51ec2c6842e5d649fcdeb18bd7
Stranik2504 <58374405+Stranik2504@users.noreply.github.com>
2026-09-20 23:02:34 +0300

    Added null check to light and overlay

--- common/src/main/java/org/figuramc/figura/model/rendering/ImmediateFiguraRenderer.java
+++ common/src/main/java/org/figuramc/figura/model/rendering/ImmediateFiguraRenderer.java
@@ -8,6 +8,7 @@ import net.minecraft.client.gui.components.debug.DebugScreenEntries;
 import net.minecraft.client.renderer.*;
 import net.minecraft.client.renderer.rendertype.RenderType;
 import net.minecraft.client.renderer.rendertype.RenderTypes;
+import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.resources.Identifier;
 import net.minecraft.util.LightCoordsUtil;
 import net.minecraft.world.level.Level;
@@ -343,8 +344,8 @@ public class ImmediateFiguraRenderer extends FiguraRenderer {
 
                 // Store calculated light level and current overlay effect before pushing pose stack
                 PartCustomization oldPeek = customizationStack.peek();
-                int light = oldPeek.light;
-                int overlay = oldPeek.overlay;
+                int light = oldPeek.light != null ? oldPeek.light : LightCoordsUtil.FULL_BRIGHT;
+                int overlay = oldPeek.overlay != null ? oldPeek.overlay : OverlayTexture.NO_OVERLAY;
 
                 FiguraVec3 pivot = custom.getPivot().copy().add(custom.getOffsetPivot());
                 pivotOffsetter.setPos(pivot);
